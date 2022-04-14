d48dd952a34bfd4b510502560b3fc43d2b681173
IgnacioTomasRamallo <85121659+IgnacioTomasRamallo@users.noreply.github.com>
2022-04-14 17:32:46 -0300

    Add files via upload

---
+++ Ejercicios/main.js
@@ -0,0 +1,28 @@
+//Verduleria
+
+let lista;
+let catalogo = prompt(`Que desea comprar hoy ?:
+-papas
+-manzanas
+-lechuga`);
+function comerciante(){
+    if(catalogo == papas){
+        lista = 'papas 1kg';
+    }else if(catalogo == manzanas){
+        lista = 'manzanas 1kg';
+    }else if(catalogo == lechuga){
+        lista = 'lechuga 1kg';
+    }else{
+        alert('Articulo no disponible.');
+    }
+}
+
+//Pago
+let tarjeta;
+let efectivo;
+
+function pagarTiket(){
+    switch(pagarTiket){
+
+    }
+}
